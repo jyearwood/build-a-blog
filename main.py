de4befcf8fdfc95ecbd9f861f9d3fbe13a76b23e
Janelle <msyearwood@yahoo.com>
2018-01-30 19:24:40 -0500

    initiating templets

--- main.py
+++ main.py
@@ -13,14 +13,35 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
-class Task(db.Model):
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime(), default=datetime.utcnow, index=True)
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-
-    def __init__(self,name):
-        slef.name = name 
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
 
-if __name__=='__main__':
-    app.run()
+@app.route("/", methods = ["POST", "GET"])
+def index():
+    blogs = Blog.query.order_by(Blog.id.desc()).all()
+    return render_template("blog.html", blogs = blogs)
+
+
+@app.route("/newpost", methods = ["POST", "GET"])
+def newpost():
+    return render_template("newpost.html")
+
+@app.route("/blog", methods = ["POST", "GET"])
+def blog():
+
+
+@app.route("/validate", methods=["POST", "GET"])
+def validate():
+
+
+
+if __name__ == '__main__':
+app.run()
