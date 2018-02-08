bc072d55150a0de021a4cc62acf5fe13ad4f1ef1
Janelle <msyearwood@yahoo.com>
2018-02-08 02:10:51 -0500

    database fix

--- main.py
+++ main.py
@@ -8,16 +8,13 @@ app = Flask(__name__)
 app.config['DEBUG']= True
 app.config['SQLALCHEMY_DATABASE_URI'] ='mysql+pymysql://build-a-blog:buildablog@localhost:3307/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
-
-
-db = SQLAlchemy(app)
+db= SQLAlchemy(app)
 
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime(), default=datetime.utcnow, index=True)
 
     def __init__(self, title, body):
         self.title = title
